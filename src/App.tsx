e1eced1393e51367093c4726425446eb2f884e9d
Pedro Marchiori Dutra <pedromarchiori007@gmail.com>
2025-08-09 15:02:52 -0300

    feat: componente DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from './components/Container';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 import { Footer } from './components/Footer';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
@@ -22,6 +23,27 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className='formRow'>
+            <DefaultInput labelText='Task' id="meuInput" type='text' placeholder='Task' disabled/>
+          </div>
+
+          <div className='formRow'>
+            <span>Lorem ipsum dolor sit amet.</span>
+          </div>
+
+          <div className='formRow'>
+            <span>Ciclos</span>
+            <span>0 0 0 0 0 0 0</span>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
 
       <Footer />
